8b4ba3f669bc05baa6bfccdfa53ef0a763650938
unknown <houdabnklf@gmail.com>
2024-01-17 17:00:36 -0800

    my movies app

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { homeData } from "../../dummyData"
+import "./footer.css"
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+       <p>© 2024 Houda bn. All Rights Reserved.</p>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
